e651033bc0ff341f6b77efbb012e62beff9764f8
chenhq <chenhongqing@llmofang.com>
2016-06-25 22:17:11 +0800

    ok

--- test/main.c
+++ test/main.c
@@ -6,56 +6,46 @@
 #include "dbf.h"
 #include "k.h"
 
-int connect_kdb(char* host, int port) {
-	int handle = khpun(host, port, "user:pass", 10000);
-	if ((!handle) || (handle < 0)) {
-		printf("Cannot connect\n");
-		exit(1);
-	}
-	else {
-		printf("connected %s:%i\n", host, port);
-	}
-	return handle;
-}
-
-int main(int argc, char* argv[])
+BOOL main_c(const char* filename)
 {
-	if (argc < 4) {
-		printf("Usage: progname tablename.dbf host port\n");
-		exit(1);
-	}
 
-	char* filename = argv[1];
-	char* host = argv[2];
-	int port = atoi(argv[3]);
+	DBF_HANDLE handle = dbf_open(filename, NULL);
+	BOOL ok = (handle != NULL);
 
-	int kdbhandle = connect_kdb(host, port);
-	
-		DBF_HANDLE dbfhandle = dbf_open(filename, NULL);
-	if (dbfhandle == NULL) {
-		printf("Open dbf error!\n");
-		exit(0);
-	}
+	if (ok)
+	{
+		size_t count = dbf_getrecordcount(handle);
+		size_t fields = dbf_getfieldcount(handle);
+		size_t i, j;
 
-	size_t old_count = 0;
-	size_t new_count = 0;
-	char cmd[1024] = { 0 };
-	
-
-	while (TRUE) {
-		new_count = dbf_getrecordcount(dbfhandle);
-		for (; old_count < new_count; old_count++) {
-			dbf_setposition(dbfhandle, old_count);
-			size_t fields = dbf_getfieldcount(kdbhandle);
-			for (int j = 0; j < fields; j++)
+		for (i = 0; i < count; i++)
+		{
+			dbf_setposition(handle, i);
+			for (j = 0; j < fields; j++)
 			{
 				char temp[80] = "";
-				dbf_getfield(kdbhandle, dbf_getfieldptr(kdbhandle, j), temp, sizeof(temp), DBF_DATA_TYPE_ANY);
+
+				dbf_getfield(handle, dbf_getfieldptr(handle, j), temp, sizeof(temp), DBF_DATA_TYPE_ANY);
 				if (j) printf(";");
 				printf(temp);
 			}
 			printf("\n");
 		}
+		dbf_close(&handle);
 	}
+	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+int main(int argc, char* argv[])
+{
+	BOOL ok = (argc > 1);
 
+	if (ok)
+	{
+		ok = main_c(argv[1]);
+	}
+	else
+	{
+		printf("Usage: demo.exe tablename.dbf\n");
+	}
+	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
